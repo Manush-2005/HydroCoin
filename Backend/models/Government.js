94abc25379ba02dbf44f0299ea2edc4aeb6d7ea5
Bhargavimachhi <140937880+Bhargavimachhi@users.noreply.github.com>
2025-08-30 04:39:12 +0530

    added signup routes

---
+++ Backend/models/Government.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const locationSchema = new mongoose.Schema({
+  lat: {
+    type: Number,
+    required: true,
+  },
+  lon: {
+    type: Number,
+    required: true,
+  }
+});
+
+let governmentSchema = new mongoose.Schema({
+  email: {
+    type: "string",
+    required: true,
+  },
+  password: {
+    type: "string",
+    required: true,
+  },
+  name: {
+    type: "string",
+    required: true,
+  },
+  address: {
+    type: "string",
+    required: true,
+  },
+  location: {
+    type: locationSchema,
+    required: true,
+  }
+});
+
+const Government = mongoose.model("Government", governmentSchema);
+export default Government;
---
+++ Backend/models/Producer.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const locationSchema = new mongoose.Schema({
+  lat: {
+    type: Number,
+    required: true,
+  },
+  lon: {
+    type: Number,
+    required: true,
+  }
+});
+
+let producerSchema = new mongoose.Schema({
+  email: {
+    type: "string",
+    required: true,
+  },
+  password: {
+    type: "string",
+    required: true,
+  },
+  name: {
+    type: "string",
+    required: true,
+  },
+  address: {
+    type: "string",
+    required: true,
+  },
+  location: {
+    type: locationSchema,
+    required: true,
+  }
+});
+
+const Producer = mongoose.model("Producer", producerSchema);
+export default Producer;
